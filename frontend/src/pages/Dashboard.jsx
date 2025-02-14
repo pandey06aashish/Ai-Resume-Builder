30642285719c4e5bdcf9f319d5e0cf690b369df0
Aashish pandey <pandey786aashish@gmail.com>
2025-02-14 12:55:08 +0530

    Setup the backend folder and install Strapi  to manage conent

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='p-10 md:px-20 lg:px-32'>
+      <h2 className='font-bold text-3xl'>My Resume</h2>
+    </div>
   )
 }
 

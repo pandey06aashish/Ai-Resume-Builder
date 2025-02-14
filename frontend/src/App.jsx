dabbf795a8063c5d49ab18afaff94d41e6f0d7ed
Aashish pandey <pandey786aashish@gmail.com>
2025-02-14 08:33:23 +0530

    setup the project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <Button> Subscribe</Button>
+
+    </h1>
+  )
+}

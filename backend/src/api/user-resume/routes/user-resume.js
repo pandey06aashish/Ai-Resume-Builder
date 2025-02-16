61a93a27b8c926e3884b24b3e1a3da1e485a66b9
Aashish pandey <pandey786aashish@gmail.com>
2025-02-16 13:52:32 +0530

    Using Strapi to manage Content of website .Setup Strapi to store data

--- backend/src/api/user-resume/routes/user-resume.js
+++ backend/src/api/user-resume/routes/user-resume.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * category router.
+ * user-resume router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::category.category');
+module.exports = createCoreRouter('api::user-resume.user-resume');
